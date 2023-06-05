e5daf2aefe88331fca71c423eeef8e734a935406
bondarenkoigor <Bond_fh54@student.itstep.org>
2023-06-05 20:22:18 +0300

    Layout and search

---
+++ Front/my-app/src/components/Search.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Search() {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState([]);
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        searchAPI();
+    };
+    const searchAPI = async () => {
+        try {
+            //   const response = await axios.get(``); //add API key
+            let tmpResults = [{ img: 'https://media.istockphoto.com/id/488580536/photo/giraffe-in-front-of-kilimanjaro-mountain.jpg?s=612x612&w=0&k=20&c=xqZ1Lo6_a1Lq4JwRFTts6xCGI0NxMI4UuAYXM5Qwk8g=' },
+            { img: 'https://media.istockphoto.com/id/488580536/photo/giraffe-in-front-of-kilimanjaro-mountain.jpg?s=612x612&w=0&k=20&c=xqZ1Lo6_a1Lq4JwRFTts6xCGI0NxMI4UuAYXM5Qwk8g=' },
+            { img: 'https://media.istockphoto.com/id/488580536/photo/giraffe-in-front-of-kilimanjaro-mountain.jpg?s=612x612&w=0&k=20&c=xqZ1Lo6_a1Lq4JwRFTts6xCGI0NxMI4UuAYXM5Qwk8g=' },]
+            setResults(tmpResults);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+    return (
+        <div className='search'>
+            <form onSubmit={handleSubmit}>
+                <input className='searchInput' type="text" value={query} onChange={handleInputChange} placeholder="Search Animal" />
+                <button className='searchButton' type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"> <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/> </svg>                 </button>
+            </form>
+            <ul className='results'>
+                {results.map((result, index) => ( //TODO: Make this a component
+                    <div className='resultCard'>
+                        <img src={result.img} key={index}></img>
+                        <p className='cardTitle'>AnimalName</p>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Search; 
---
+++ Front/my-app/src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>Kingdom</li>
+                <li>Subspecies</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SideBar;
