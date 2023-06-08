8b512834b5d8b4d585061df82d6fa7ec8247c1c4
bondarenkoigor <Bond_fh54@student.itstep.org>
2023-06-08 10:22:34 +0300

    Admin connected to API

--- Front/my-app/src/pages/AdminPage.js
+++ Front/my-app/src/pages/AdminPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 import '../css/Admin.css'
+import axios from "axios";
+import { Outlet, useNavigate} from "react-router-dom";
 
 function AdminPage() {
+
+    const navigate = useNavigate();
+
+
     return (
         <div className="admin-page">
             <h1>Wildlife Admin</h1>
@@ -11,89 +17,39 @@ function AdminPage() {
                         <ul>
                             <li><p>Kingdoms</p>
                                 <div className="dropdown-content">
-                                    <button>Add</button>
-                                    <button>Edit</button>
+                                    <button onClick={() => navigate('kingdom/add')}>Add</button>
+                                    <button onClick={() => navigate('kingdom/edit')}>Edit</button>
+                                    <button onClick={() => navigate('kingdom/remove')}>Remove</button>
                                 </div></li>
                             <li><p>Classes</p>
                                 <div className="dropdown-content">
-                                    <button>Add</button>
-                                    <button>Edit</button>
+                                    <button onClick={() => navigate('class/add')}>Add</button>
+                                    <button onClick={() => navigate('class/edit')}>Edit</button>
+                                    <button onClick={() => navigate('class/remove')}>Remove</button>
                                 </div></li>
                             <li><p>Families</p>
                                 <div className="dropdown-content">
-                                    <button>Add</button>
-                                    <button>Edit</button>
+                                    <button onClick={() => navigate('family/add')}>Add</button>
+                                    <button onClick={() => navigate('family/edit')}>Edit</button>
+                                    <button onClick={() => navigate('family/remove')}>Remove</button>
                                 </div></li>
-                            <li><p>Genes</p>
+                            <li><p>Genuses</p>
                                 <div className="dropdown-content">
-                                    <button>Add</button>
-                                    <button>Edit</button>
+                                    <button onClick={() => navigate('genus/add')}>Add</button>
+                                    <button onClick={() => navigate('genus/edit')}>Edit</button>
+                                    <button onClick={() => navigate('genus/remove')}>Remove</button>
                                 </div></li>
                             <li><p>Species</p>
                                 <div className="dropdown-content">
-                                    <button>Add</button>
-                                    <button>Edit</button>
+                                    <button onClick={() => navigate('species/add')}>Add</button>
+                                    <button onClick={() => navigate('species/edit')}>Edit</button>
+                                    <button onClick={() => navigate('species/remove')}>Remove</button>
                                 </div></li>
                         </ul>
                     </div>
-                    <div className="inputs">
-                        <div className="main-inputs">
-                            <ul>
-                                <li>
-                                    <label>Input Name:</label>
-                                    <input type="text"></input>
-                                </li>
-                                <li>
-                                    <label>Input Description:</label>
-                                    <input type="text"></input>
-                                </li>
-                                <li>
-                                    <label>Image URL:</label>
-                                    <input type="text"></input>
-                                </li>
-
-                            </ul>
-                        </div>
-                        <div className="category-inputs">
-                            <ul>
-                                <li>
-                                    <label>Kingdom:</label>
-                                    <form action="">
-                                        <select>
-
-                                        </select>
-                                    </form>
-                                </li>
-                                <li>
-                                    <label>Gene:</label>
-                                    <form action="">
-                                        <select>
-
-                                        </select>
-                                    </form>
-                                </li>
-                                <li>
-                                    <label>Class:</label>
-                                    <form action="">
-                                        <select>
-
-                                        </select>
-                                    </form>
-                                </li>
-                                <li>
-                                    <label>Family:</label>
-                                    <form action="">
-                                        <select>
-
-                                        </select>
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
+                    <Outlet></Outlet>
                 </div>
-                <button className="confirm-button">Confirm</button>
+                
             </div>
         </div>
     );
